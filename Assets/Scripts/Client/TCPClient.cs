f6d2cc6dbae0ab581538a9cd6e9c1779c0176e3a
mukilimmj@gmail.com <mukilimmj@gmail.com>
2025-03-25 18:06:44 -0400

    merge conflict

--- Assets/Scripts/Client/TCPClient.cs
+++ Assets/Scripts/Client/TCPClient.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-<<<<<<< HEAD
-=======
-using TMPro;
->>>>>>> a5b5878046e047f85a9bc581eb7047349dbe6639
 using UnityEngine;
 
 public class TCPClient : MonoBehaviour
@@ -21,8 +17,6 @@ public class TCPClient : MonoBehaviour
 
     [SerializeField]
     private int port = 13000;
-
-    public TextMeshProUGUI debugText;
 
     //[SerializeField]
     //private TextMeshProUGUI debugText;
@@ -48,8 +42,9 @@ public class TCPClient : MonoBehaviour
     #endregion
 
     // create Singleton object before threads are created. 
-    void Awake() {
-        
+    void Awake()
+    {
+
     }
     void Start()
     {
@@ -57,7 +52,7 @@ public class TCPClient : MonoBehaviour
         {
             client = new TcpClient(serverIPAddress, port);
             Debug.Log($"TCP client listening on port {port}");
-        } 
+        }
         catch (Exception e)
         {
             Debug.Log(e);
@@ -75,59 +70,63 @@ public class TCPClient : MonoBehaviour
         return serverIPAddress;
     }
 
-    private void Message(int clientId, string message) {
+    private void Message(int clientId, string message)
+    {
         //Debug.Log($"{clientId} {message}");
         //debugText.text += $"{message} \n";
     }
 
 
 
-    public int GetReceivestatus() {
+    public int GetReceivestatus()
+    {
         return messagestatus;
     }
-    public void ResetMessagestatus() {
+    public void ResetMessagestatus()
+    {
     }
 
-/*** 
- * Received Message format:
- * 
- * TCP message type - int
- * object count - int
- * total num of bytes of the table - int
- * table data
- * 
- * ***/
+    /*** 
+     * Received Message format:
+     * 
+     * TCP message type - int
+     * object count - int
+     * total num of bytes of the table - int
+     * table data
+     * 
+     * ***/
     private void ListenToServer(TcpClient server)
+    {
+        try
         {
-            try
-            {
-                NetworkStream stream = server.GetStream();
-                
-                while(true) {
-                    byte[] buffer = new byte[1024 * 1024]; // 1MB buffer
-                    int byteCount = stream.Read(buffer, 0, buffer.Length);
-                    byte[] dataTemp = new byte[byteCount];
-                    Buffer.BlockCopy(buffer, 0, dataTemp, 0, byteCount);
+            NetworkStream stream = server.GetStream();
 
-                    // string response = System.Text.Encoding.ASCII.GetString(dataTemp, 0, bytes);
-                    UnityDispatcher.Instance.Enqueue(() => HandleMessage(dataTemp));
-                    
-                    //Dispatcher.Instance.Enqueue(() => Message(clientId, $"Recieved : {byteCount}"));
-                    //dispatcher.Enqueue(() => Message(clientId, $"Message : " +
-                    //    $"{System.Text.Encoding.ASCII.GetString(dataTemp, 0, MessageType.MESSAGELENGTH)}"));
-                    //nfClient.readyForNextData = false;
-                }
-                    // Dispatcher.Instance.Enqueue(() => Message(clientId, $"Disconnects"));
-                    // stream.Close();
-                    // client.Close();
-            }
-            catch(Exception e)
+            while (true)
             {
-                Debug.Log($"Exception : {e}");
-            }
+                byte[] buffer = new byte[1024 * 1024]; // 1MB buffer
+                int byteCount = stream.Read(buffer, 0, buffer.Length);
+                byte[] dataTemp = new byte[byteCount];
+                Buffer.BlockCopy(buffer, 0, dataTemp, 0, byteCount);
 
-            Console.Read();
+                // string response = System.Text.Encoding.ASCII.GetString(dataTemp, 0, bytes);
+                UnityDispatcher.Instance.Enqueue(() => HandleMessage(dataTemp));
+
+                //Dispatcher.Instance.Enqueue(() => Message(clientId, $"Recieved : {byteCount}"));
+                //dispatcher.Enqueue(() => Message(clientId, $"Message : " +
+                //    $"{System.Text.Encoding.ASCII.GetString(dataTemp, 0, MessageType.MESSAGELENGTH)}"));
+                //nfClient.readyForNextData = false;
+            }
+            // Dispatcher.Instance.Enqueue(() => Message(clientId, $"Disconnects"));
+            // stream.Close();
+            // client.Close();
         }
+        catch (Exception e)
+        {
+            Debug.Log($"Exception : {e}");
+        }
+
+        Console.Read();
+    }
 
     private void HandleMessage(byte[] message)
     {
@@ -148,23 +147,19 @@ public class TCPClient : MonoBehaviour
 
         if (table_data.Count < totalBytes)
         {
-            debugText.text += $"have received {table_data.Count} / {totalBytes}\n";   
+
             table_data.AddRange(message);
         }
 
         if (table_data.Count == totalBytes)
         {
-            byte[] table_data_array = table_data.ToArray();
-            debugText.text += $"have received all\n";
             cursor = sizeof(int) * 3;
 
             objectHolders = new ObjectHolder[totalObjectNum];
 
             for (int i = 0; i < totalObjectNum; i++)
             {
-                Debug.Log($"TCP: {i} parsing table {cursor}");
                 objectHolders[i] = new ObjectHolder();
-<<<<<<< HEAD
                 objectHolders[i].position = new Vector3(BitConverter.ToSingle(table_data.ToArray(), cursor), BitConverter.ToSingle(table_data.ToArray(), cursor += sizeof(float)),
                     BitConverter.ToSingle(table_data.ToArray(), cursor += sizeof(float)));
                 objectHolders[i].eulerAngles = new Vector3(BitConverter.ToSingle(table_data.ToArray(), cursor += sizeof(float)), BitConverter.ToSingle(table_data.ToArray(), cursor += sizeof(float)),
@@ -175,18 +170,6 @@ public class TCPClient : MonoBehaviour
                 objectHolders[i].totalTriChunkNum = BitConverter.ToInt32(table_data.ToArray(), cursor += sizeof(int));
                 objectHolders[i].totalVertNum = BitConverter.ToInt32(table_data.ToArray(), cursor += sizeof(int));
                 objectHolders[i].submeshCount = BitConverter.ToInt32(table_data.ToArray(), cursor += sizeof(int));
-=======
-                objectHolders[i].position = new Vector3(BitConverter.ToSingle(table_data_array, cursor), BitConverter.ToSingle(table_data_array, cursor += sizeof(float)),
-                    BitConverter.ToSingle(table_data_array, cursor += sizeof(float)));
-                objectHolders[i].eulerAngles = new Vector3(BitConverter.ToSingle(table_data_array, cursor += sizeof(float)), BitConverter.ToSingle(table_data_array, cursor += sizeof(float)),
-                    BitConverter.ToSingle(table_data_array, cursor += sizeof(float)));
-                objectHolders[i].scale = new Vector3(BitConverter.ToSingle(table_data_array, cursor += sizeof(float)), BitConverter.ToSingle(table_data_array, cursor += sizeof(float)),
-                    BitConverter.ToSingle(table_data_array, cursor += sizeof(float)));
-                objectHolders[i].totalVertChunkNum = BitConverter.ToInt32(table_data_array, cursor += sizeof(float));
-                objectHolders[i].totalTriChunkNum = BitConverter.ToInt32(table_data_array, cursor += sizeof(int));
-                objectHolders[i].totalVertNum = BitConverter.ToInt32(table_data_array, cursor += sizeof(int));
-                objectHolders[i].submeshCount = BitConverter.ToInt32(table_data_array, cursor += sizeof(int));
->>>>>>> a5b5878046e047f85a9bc581eb7047349dbe6639
                 cursor += sizeof(int);
 
                 // TODO: Currently not used for the isvisible and isowned
@@ -197,42 +180,38 @@ public class TCPClient : MonoBehaviour
                 //Transform transform = objectsInScene[i].transform;
                 for (int j = 0; j < objectHolders[i].submeshCount; j++)
                 {
-<<<<<<< HEAD
                     int materialNameLength = BitConverter.ToInt32(table_data.ToArray(), cursor);
                     objectHolders[i].materialNames[j] = Encoding.ASCII.GetString(table_data.ToArray(), cursor += sizeof(int), materialNameLength);
-=======
-                    int materialNameLength = BitConverter.ToInt32(table_data_array, cursor);
-                    Debug.Log($"TCP: {table_data_array.Length}, {cursor}, {materialNameLength}");
-                    objectHolders[i].materialNames[j] = Encoding.ASCII.GetString(table_data_array, cursor += sizeof(int), materialNameLength);
->>>>>>> a5b5878046e047f85a9bc581eb7047349dbe6639
                     cursor += materialNameLength;
 
                     //Debug.Log($"ObjectID{i} - {objectHolders[i].materialNames[j]}");
                 }
-                Debug.Log($"TCP: {i} finished parsing table {cursor}");
             }
 
             OnReceivedServerTable?.Invoke();
         }
 
-        
+
     }
 
-    public void SendString(string message) {
+    public void SendString(string message)
+    {
         Debug.Log(message);
         byte[] startmessage = System.Text.Encoding.ASCII.GetBytes(message);
         client.GetStream().Write(startmessage, 0, startmessage.Length);
     }
 
-    public void SendMessage(byte[] message) {
+    public void SendMessage(byte[] message)
+    {
         try
         {
             client.GetStream().Write(message, 0, message.Length);
-        } catch (Exception e)
+        }
+        catch (Exception e)
         {
             Debug.Log(e);
         }
-        
+
     }
 
     public void OnApplicationQuit()
